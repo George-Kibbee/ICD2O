3085493c3120c3a94f4e8b03fe6fa452538e91ec
George-Kibbee <George.kibbee@stu.ocsb.ca>
2025-09-24 18:12:57 +0000

    did more work t=for the final for unit 1

---
+++ Unit1/Final/JavaScript/script.js
@@ -0,0 +1,11 @@
+document.getElementById('clearbutton').addEventListener('click', () => {
+  document.getElementById('userinput').value = '';
+  document.getElementById("userinput").focus();
+});
+const button = document.getElementById('searchbutton');
+const input = document.getElementById('userinput');
+button.addEventListener('click', function () {
+  const q = (input.value || '').trim();
+  const url = 'https://www.google.com/search?q=' + encodeURIComponent(q);
+  window.open(url, '_blank');
+});
