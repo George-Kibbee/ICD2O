7394b92497f514a35e1f5b59326d13556346f862
George-Kibbee <George.kibbee@stu.ocsb.ca>
2025-09-25 18:07:58 +0000

    did more work for the final

--- Unit1/Final/JavaScript/script.js
+++ Unit1/Final/JavaScript/script.js
@@ -4,6 +4,7 @@ document.getElementById('clearbutton').addEventListener('click', () => {
 });
 const button = document.getElementById('searchbutton');
 const input = document.getElementById('userinput');
+
 button.addEventListener('click', function () {
   const q = (input.value || '').trim();
   const url = 'https://www.google.com/search?q=' + encodeURIComponent(q);
